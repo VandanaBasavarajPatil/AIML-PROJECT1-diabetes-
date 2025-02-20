f2f7a63f982622eb23e60f7c420b4ae6aca59b48
VandanaBasavarajPatil <vandanabasavaraj6@gmail.com>
2025-02-20 12:30:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 
 st.set_page_config(page_title="Diabetes Prediction", layout="wide", page_icon="🧑‍⚕")
 
-diabetes_model_path = r"C:\\Users\\user\\OneDrive\\Desktop\\pro diesease\\diabetes_model.sav"
+diabetes_model_path = r"diabetes_model.sav"
 diabetes_model = pickle.load(open(diabetes_model_path,'rb'))
 
 st.title ('Diabetes Prediction using ML')
